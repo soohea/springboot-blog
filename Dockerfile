c38ca69ebd3663986a0225863f196aa594ed0e2a
soohea <sooheaa@gmail.com>
2022-05-28 14:37:58 +0800

    integration test

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM openjdk-8u111-alpine
--- src/test/java/com/github/soohea/blog/integration/MyIntegrationTest.java
+++ src/test/java/com/github/soohea/blog/integration/MyIntegrationTest.java
@@ -1,7 +1,13 @@
 package com.github.soohea.blog.integration;
 
+import com.github.soohea.blog.Application;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MyIntegrationTest {
     @Test
     public void myIntegrationTest(){
